e2bf8de045952344df050cf877ef1d6243fda819
Ivar Clemens <post@ivarclemens.nl>
2015-11-23 15:09:52 +0000

    Support for keyboard.

--- task.js
+++ task.js
@@ -1,9 +1,11 @@
 
-experimentFrontendControllers.controller('RMET', ['$scope', '$http', '$cookies',
-  function($scope, $http, $cookies)
+experimentFrontendControllers.controller('RMET', ['$scope', '$http', '$cookies', '$controller',
+  function($scope, $http, $cookies, $controller)
   {
     $scope.resources = $scope.screen.root;
     $scope.prefix += "RMET.";
+
+    $controller('Keyboard', {$scope: $scope});
 
     // List of all trials
     $scope.trial = {};
@@ -14,12 +16,91 @@ experimentFrontendControllers.controller('RMET', ['$scope', '$http', '$cookies',
     $scope.descriptions = {};
 
 
-    $scope.set_trial = function(trial) {
+    /**
+     * Return index (0..3) of the current response
+     */
+    function get_response_index()
+    {
+      var key = $scope.prefix + $scope.trial.id;
+      
+      // Options
+      var options = $scope.trial.options;      
+      
+      // Find ID of response given value
+      if(key in $scope.responses) {
+        // Current response
+        var response = $scope.responses[key];       
+        
+        // Loop over options and return index if matches
+        for(var i = 0; i < options.length; i++) {
+          if(options[i] == response) {
+            return i;
+          }
+        }        
+      }      
+
+      return undefined;
+    }
+
+
+    /**
+     * Allow number keys (1..4) and arrow keys to 
+     * navigate through options.
+     */
+    $scope.handle_keyboard_event = function(evt)
+    {
+      var key = $scope.prefix + $scope.trial.id;      
+      var value = get_response_index();
+            
+      // Set response using keys 1..4
+      if(evt.key >= "1" && evt.key <= "4")
+      {
+        var option = $scope.trial.options[evt.key - 1];        
+        $scope.responses[key] = option;
+        return;
+      }
+      
+      // Allow the use of arrow keys
+      if(evt.key == 'ArrowLeft')
+      {
+        if(value == undefined) value = 0;
+        if(value == 1) value = 0;
+        if(value == 3) value = 2;
+      }
+      
+      if(evt.key == 'ArrowRight')
+      {
+        if(value == undefined) value = 1;
+        if(value == 0) value = 1;
+        if(value == 2) value = 3;
+      }
+           
+      if(evt.key == 'ArrowUp')
+      {
+        if(value == undefined) value = 0;
+        if(value == 2) value = 0;
+        if(value == 3) value = 1;
+      }
+      
+      if(evt.key == 'ArrowDown')
+      {
+        if(value == undefined) value = 2;
+        if(value == 0) value = 2;
+        if(value == 1) value = 3;
+      }
+
+      $scope.responses[key] = $scope.trial.options[value];
+    }
+
+
+    $scope.set_trial = function(trial) 
+    {
       $scope.responses[ $scope.prefix + 'Page' ] = trial;
     }
 
 
-    $scope.get_trial = function() {
+    $scope.get_trial = function() 
+    {
       var trial = $scope.responses[ $scope.prefix + 'Page' ];
 
       if(trial === undefined) {
@@ -53,7 +134,8 @@ experimentFrontendControllers.controller('RMET', ['$scope', '$http', '$cookies',
     /**
      * Keeps track of when a user requests the definition of an emotion
      */
-    $scope.mark_definition_request = function() {
+    $scope.mark_definition_request = function() 
+    {
       var key = $scope.prefix + 'Definition.' + $scope.emotion;
 
       if(!(key in $scope.responses)) {
@@ -64,7 +146,8 @@ experimentFrontendControllers.controller('RMET', ['$scope', '$http', '$cookies',
     }
 
 
-    $scope.is_next_allowed = function() {
+    $scope.is_next_allowed = function() 
+    {
       if($scope.get_trial() in $scope.trials)
       {
         var key = $scope.trials[$scope.get_trial()].id;
@@ -77,7 +160,8 @@ experimentFrontendControllers.controller('RMET', ['$scope', '$http', '$cookies',
     /**
      * Move to the next trial
      */
-    $scope.next = function() {
+    $scope.next = function() 
+    {
       if(!$scope.is_next_allowed() && !debug)
         return;
 
@@ -97,7 +181,8 @@ experimentFrontendControllers.controller('RMET', ['$scope', '$http', '$cookies',
     /**
      * Move to previous trial
      */
-    $scope.previous = function() {
+    $scope.previous = function() 
+    {
       if(!$scope.is_previous_allowed() && !debug)
         return;
 
