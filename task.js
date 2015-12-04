12c9fd1438aa89d16610eed7aa6cedc68b990bf8
Ivar Clemens <post@ivarclemens.nl>
2015-12-04 11:42:00 +0000

    Preloading images in RMET task.

--- task.js
+++ task.js
@@ -96,9 +96,30 @@ experimentFrontendControllers.controller('RMET', ['$scope', '$http', '$cookies',
     }
 
 
+    /**
+     * Changes the current trial
+     * 
+     * trial - number of the trial to change to
+     * return - True on success, false on failure
+     */
     $scope.set_trial = function(trial)
     {
       $scope.responses[ $scope.prefix + 'Page' ] = trial;
+      $scope.trial = $scope.trials[$scope.get_trial()];
+      
+      var canvas = document.getElementById("image");
+      
+      if(canvas === undefined)
+        return false;
+      
+      var context = canvas.getContext("2d");
+      
+      if(context === undefined)
+        return false;
+
+      context.drawImage($scope.trial.image, 0, 0, canvas.clientWidth, canvas.clientHeight);
+      
+      return true;
     }
 
 
@@ -114,16 +135,42 @@ experimentFrontendControllers.controller('RMET', ['$scope', '$http', '$cookies',
       return parseInt(trial);
     }
 
-
+        
     /**
-     * Populate list of trials
+     * Load stimuli and call oncomplete callback
      */
-    $http.get($scope.resources + 'Data/Stimuli.json').
-        success(function (data, status) {
-          $scope.trials = data;
-          $scope.trial = data[$scope.get_trial()];
-        });
-
+    $scope.load_stimuli = function(complete_callback)
+    {
+      return new Promise(function(resolve, reject)
+      {
+        $http.get($scope.resources + 'Data/Stimuli.json').
+            success(function (data, status) {
+            
+              var itemsLeft = data.length;
+            
+              // Preload all images           
+              for(var i in data) {
+                var path = $scope.resources + 'Faces/' + data[i].image;
+                              
+                data[i].image = new Image();              
+                data[i].image.onload = function() {
+                  itemsLeft--;
+                  
+                  if(itemsLeft == 0)
+                    resolve();
+                }.bind(this);
+                
+                data[i].image.onerror = function() { reject(); };              
+                data[i].image.src = path;
+              }
+              
+              // Add trials to scope
+              $scope.trials = data;
+              $scope.trial = data[$scope.get_trial()];          
+            });
+      }.bind(this));
+    }
+    
 
     /**
      * Populate list of definitions
@@ -139,7 +186,8 @@ experimentFrontendControllers.controller('RMET', ['$scope', '$http', '$cookies',
      */
     $scope.mark_definition_request = function()
     {
-      var key = $scope.prefix + 'Definition.' + $scope.emotion;
+      var definition = $scope.responses[$scope.prefix + 'Definition'];
+      var key = $scope.prefix + 'Definition.' + definition;
 
       if(!(key in $scope.responses)) {
         $scope.responses[key] = 0;
@@ -160,6 +208,7 @@ experimentFrontendControllers.controller('RMET', ['$scope', '$http', '$cookies',
       }
     }
 
+
     /**
      * Move to the next trial
      */
@@ -175,8 +224,7 @@ experimentFrontendControllers.controller('RMET', ['$scope', '$http', '$cookies',
         $scope.update_balance(id, 0.10);
       } else {
         $scope.responses[$scope.prefix + 'Definition'] = "";
-        $scope.set_trial($scope.get_trial() + 1);
-        $scope.trial = $scope.trials[$scope.get_trial()];
+        $scope.set_trial($scope.get_trial() + 1);        
       }
     }
 
@@ -195,12 +243,19 @@ experimentFrontendControllers.controller('RMET', ['$scope', '$http', '$cookies',
         if($scope.get_trial() in $scope.trials)
         {
           $scope.responses[$scope.prefix + 'Definition'] = "";
-          $scope.set_trial($scope.get_trial() - 1);
-          $scope.trial = $scope.trials[$scope.get_trial()];
+          $scope.set_trial($scope.get_trial() - 1);          
         } else {
           return false;
         }
       }
     }
+    
+    
+    /**
+     * Load stimuli and mark them as loaded
+     */
+    $scope.load_stimuli().then(function() {
+      $scope.set_trial(0);
+    });
   }
 ]);
